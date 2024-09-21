d75d07f9b321b9c84bd3bf01c03e581147c75193
Ayaan <ayaan2536@gmail.com>
2024-09-21 11:52:31 +0530

    first commit

---
+++ src/test/java/com/working/BasketInvestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.working;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketInvestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

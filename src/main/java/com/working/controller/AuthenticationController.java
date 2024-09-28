552a4adf4c2301e9358260839a1f23b61b6d9bbb
Ganesh Ram B <76607195+ganeshramb@users.noreply.github.com>
2024-09-28 18:29:26 +0530

    add authentication final

---
+++ src/main/java/com/working/controller/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.working.controller;
+
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+   
+    @GetMapping("/login")
+    public String login(Principal principal, Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("errorMsg", "Your username or password is incorrect.");
+        }
+
+        if (logout != null) {
+            model.addAttribute("msg", "You have been logged out successfully.");
+        }
+        
+        System.out.println(principal.getName());
+        
+        return "login"; 
+    }
+
+
+    @GetMapping("/logoutPage")
+    public String logoutPage(Authentication authentication, Model model) {
+        String username = "";
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                username = ((UserDetails) principal).getUsername();
+            }
+        }
+        model.addAttribute("username", username);
+        return "logout";  
+    }
+    
+    
+}
---
+++ src/main/java/com/working/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package com.working.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.working.model.Users;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByUsername(String username);
+}
